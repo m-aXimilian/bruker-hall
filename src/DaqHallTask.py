fd8fe1416320bc14a3b2836307f900caee185b04
Maximilian Küffner <maximilian.kueffner@tum.de>
2022-03-04 15:31:54 +0100

    black auto format all

--- src/DaqHallTask.py
+++ src/DaqHallTask.py
@@ -7,27 +7,28 @@ sys.path.append(parentdir + "/fmr-py/src")
 
 import visa_devices as dev
 
+
 class DaqHallTask(dev.NIUSB6259):
     """Additions to the NIUSB6259 class."""
+
     def __init__(self) -> None:
         super().__init__()
         self.task_name = self.task.channel_names
 
     def singleRead(self):
         """Read a single value from all ai-channels."""
-        if not self.task.channel_names[0].find("ai"): 
+        if not self.task.channel_names[0].find("ai"):
             logging.DEBUG("DAQ read failure")
             raise TypeError("Cannot read on a task without registered inputs")
         return self.task.read()
 
     def singleWrite(self, v):
         """Write a single value to an output channel.
-        
+
         Args:
-        	v (num): value to write.
+                v (num): value to write.
         """
         if not self.task.channel_names[0].find("ao"):
             logging.DEBUG("DAQ write failure")
             raise TypeError("Cannot write -> no output channel registerd")
         self.task.write(v)
- 
